34ba17c2df071058d7c372a7bda00cb864a9c503
impact-eintr <yixingwei4@gmail.com>
2021-07-19 20:45:26 +0800

    []byte 与 string 转换的函数

--- encoding/byte2string.go
+++ encoding/byte2string.go
@@ -4,12 +4,12 @@ import (
 	"unsafe"
 )
 
-func str2bytes(s string) []byte {
+func Str2bytes(s string) []byte {
 	x := (*[2]uintptr)(unsafe.Pointer(&s))
 	h := [3]uintptr{x[0], x[1], x[1]}
 	return *(*[]byte)(unsafe.Pointer(&h))
 }
 
-func bytes2str(b []byte) string {
+func Bytes2str(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
 }
